16b0af908be2a2f3ebf3442cac06afd46d32b8b5
LewinGerber <57451950+LewinGerber@users.noreply.github.com>
2021-03-03 15:06:44 +0100

    video basic implementation

--- video/src/App.js
+++ video/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import video from './video/video.mp4';
 
 function App() {
   return (
     <div className="app">
-      
+      <video id="video" width="960" height="540" controls>
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 }
