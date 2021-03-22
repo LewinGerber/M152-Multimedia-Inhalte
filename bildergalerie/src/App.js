8b9afdeeb3a0f6a33b72a54d176616e0530b4b7d
lewinGerber <lewin.gerber@stud.kbw.ch>
2021-03-22 18:30:53 +0100

    boring gallery setup

--- bildergalerie/src/App.js
+++ bildergalerie/src/App.js
@@ -1,32 +1,71 @@
 import Slider from 'react-animated-slider';
 import horizontalCss from 'react-animated-slider/build/horizontal.css';
-import Gallery from 'react-grid-gallery';
+
+import { Gallery, Item } from 'react-photoswipe-gallery';
+import 'photoswipe/dist/photoswipe.css';
+import 'photoswipe/dist/default-skin/default-skin.css';
 import './App.css';
 
 function App() {
+  /*const getImage = async () => {
+    let imageArray = [];
+    for(var i = 0; i <= 3; i++) {
+      let imageData = await fetch('https://random.imagecdn.app/800/500');
+      imageArray.push({url: imageData.url});
+    }
+    return [...imageArray];
+  }*/
 
-  const IMAGES =
-    [{
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 174,
-      caption: "After Rain (Jeshu John - designerspics.com)"
+  const images = [
+    {
+      url: 'https://images.unsplash.com/photo-1616428329911-59581d132558?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&h=800',
+      thumbnail: 'https://images.unsplash.com/photo-1616428329911-59581d132558?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 500,
+      height: 800
     },
     {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212,
-      caption: "Boats (Jeshu John - designerspics.com)"
-    },
-
+      url: 'https://images.unsplash.com/photo-1616422285623-13ff0162193c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=672&h=850',
+      thumbnail: 'https://images.unsplash.com/photo-1616422285623-13ff0162193c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 672,
+      height: 850
+    }, 
     {
-      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-      thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-      thumbnailWidth: 320,
-      thumbnailHeight: 212
-    }]
+      url: 'https://images.unsplash.com/photo-1616418534243-ab757ff8ce3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&h=850',
+      thumbnail: 'https://images.unsplash.com/photo-1616418534243-ab757ff8ce3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 1350,
+      height: 850
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1610971441253-ab06498a0749?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&h=850',
+      thumbnail: 'https://images.unsplash.com/photo-1610971441253-ab06498a0749?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=350&h=450',
+      width: 634,
+      height: 850
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1616300461757-f4a163268869?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&h=850',
+      thumbnail: 'https://images.unsplash.com/photo-1616300461757-f4a163268869?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 633,
+      height: 850
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1616321521147-8f1b57ea97d5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&h=800',
+      thumbnail: 'https://images.unsplash.com/photo-1616321521147-8f1b57ea97d5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 700,
+      height: 800
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1616338968156-76ff58fa9197?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=603&h=800',
+      thumbnail: 'https://images.unsplash.com/photo-1616338968156-76ff58fa9197?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 603,
+      height: 800
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1616176876327-ad3577f8c230?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&h=850',
+      thumbnail: 'https://images.unsplash.com/photo-1616176876327-ad3577f8c230?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
+      width: 634,
+      height: 850
+    },
+  ];
 
   return (
     <div className="App">
@@ -41,11 +80,22 @@ function App() {
         </Slider>
       </div>
       <div id="gallery-area">
-        <Gallery 
-          images={IMAGES}
-          enableImageSelection={false}
-          backdropClosesModal={true}
-        />
+        <Gallery>
+          {
+            images.map((image) =>
+                <Item
+                  original={image.url}
+                  thumbnail={image.thumbnail}
+                  width={image.width}
+                  height={image.height}
+                >
+                  {({ ref, open }) => (
+                    <img ref={ref} id="gallery-item" onClick={open} src={image.thumbnail} />
+                  )}
+                </Item>
+            )
+          }
+        </Gallery>
       </div>
     </div>
   );
