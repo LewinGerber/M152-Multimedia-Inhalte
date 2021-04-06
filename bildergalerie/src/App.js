78a9a0c22a26ce00966a4f552b219cc8e4ec0a20
LewinGerber <lewin.gerber@stud.kbw.ch>
2021-04-06 20:53:07 +0200

    gallery attempt at thumbnail

--- bildergalerie/src/App.js
+++ bildergalerie/src/App.js
@@ -1,18 +1,34 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import Slider from 'react-animated-slider';
 import horizontalCss from 'react-animated-slider/build/horizontal.css';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import { SiUnsplash } from 'react-icons/si';
 import { FaCamera } from 'react-icons/fa';
-import { BsImages } from 'react-icons/bs';
 import 'photoswipe/dist/photoswipe.css';
 import 'photoswipe/dist/default-skin/default-skin.css';
 import './App.css';
 
-function App() {
-  let fileReader = new FileReader();
-  const [pictures, setPictures] = useState([]);
+import unsplashImages from './images/unsplash.js';
+import { imageArray } from './images/canvasResize.js';
 
+function App() {
+  //images for the slider 
+  const sliderImages = [
+    {url: 'https://s18.postimg.cc/9vhgup22x/img1.jpg'},
+    {url: 'https://s18.postimg.cc/tdc4amjl5/img3.jpg'},
+    {url: 'https://s18.postimg.cc/vunvhvvrt/img2.jpg'},
+  ]
+
+  // images from unsplash
+  const images = unsplashImages;
+
+  // uploaded images
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  // self created images
+  const selfCreatedImages = imageArray;
+
+  // read file from upload
   function readFile(e) {
     var image = new Image();
     const files = e.target.files[0];
@@ -21,9 +37,8 @@ function App() {
       fileReader.readAsDataURL(files);
       fileReader.addEventListener("load", function () {
         image.src = this.result;
-
         image.onload = () => {
-          setPictures([...pictures,
+          setUploadedImages([...uploadedImages,
           {
             src: image.src,
             width: image.width,
@@ -31,61 +46,9 @@ function App() {
           }
           ]);
         };
-
       });
     }
   }
-
-  const images = [
-    {
-      url: 'https://images.unsplash.com/photo-1616428329911-59581d132558?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&h=800',
-      thumbnail: 'https://images.unsplash.com/photo-1616428329911-59581d132558?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 500,
-      height: 800
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1616422285623-13ff0162193c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=672&h=850',
-      thumbnail: 'https://images.unsplash.com/photo-1616422285623-13ff0162193c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 672,
-      height: 850
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1616418534243-ab757ff8ce3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&h=850',
-      thumbnail: 'https://images.unsplash.com/photo-1616418534243-ab757ff8ce3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 1350,
-      height: 850
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1610971441253-ab06498a0749?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&h=850',
-      thumbnail: 'https://images.unsplash.com/photo-1610971441253-ab06498a0749?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=350&h=450',
-      width: 634,
-      height: 850
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1616300461757-f4a163268869?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&h=850',
-      thumbnail: 'https://images.unsplash.com/photo-1616300461757-f4a163268869?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 633,
-      height: 850
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1616321521147-8f1b57ea97d5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&h=800',
-      thumbnail: 'https://images.unsplash.com/photo-1616321521147-8f1b57ea97d5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 700,
-      height: 800
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1616338968156-76ff58fa9197?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=603&h=800',
-      thumbnail: 'https://images.unsplash.com/photo-1616338968156-76ff58fa9197?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 603,
-      height: 800
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1616176876327-ad3577f8c230?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&h=850',
-      thumbnail: 'https://images.unsplash.com/photo-1616176876327-ad3577f8c230?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&h=450',
-      width: 634,
-      height: 850
-    },
-  ];
 
   return (
     <div className="App">
@@ -94,9 +57,9 @@ function App() {
       </div>
       <div id="slider-area">
         <Slider classNames={horizontalCss}>
-          <div style={{ background: 'url(https://s18.postimg.cc/9vhgup22x/img1.jpg) no-repeat center center', backgroundSize: 'cover' }} />
-          <div style={{ background: 'url(https://s18.postimg.cc/tdc4amjl5/img3.jpg) no-repeat center center', backgroundSize: 'cover' }} />
-          <div style={{ background: 'url(https://s18.postimg.cc/vunvhvvrt/img2.jpg) no-repeat center center', backgroundSize: 'cover' }} />
+          {sliderImages.map((image) => 
+              <div style={{ background: 'url('+ image.url +') no-repeat center center', backgroundSize: 'cover' }} />
+          )}
         </Slider>
       </div>
       <div id="gallery-area">
@@ -117,9 +80,8 @@ function App() {
                 </Item>
               )
             }
-
             {
-              pictures.map((picture) =>
+              uploadedImages.map((picture) =>
                 <Item
                   original={picture.src}
                   thumbnail={picture}
@@ -137,8 +99,9 @@ function App() {
         <h2 className="subtitle"><FaCamera style={{marginRight: '1rem'}}/>Eigene Bilder</h2>
         <div id="gallery-wrapper">
           <Gallery>
-            {
-              images.map((image) =>
+            {console.log(selfCreatedImages)}
+            { 
+              selfCreatedImages.map((image) =>
                 <Item
                   original={image.url}
                   thumbnail={image.thumbnail}
@@ -146,7 +109,9 @@ function App() {
                   height={image.height}
                 >
                   {({ ref, open }) => (
-                    <img ref={ref} id="gallery-item" onClick={open} src={image.thumbnail} />
+                    <div id="gallery-item-wrapper">
+                      <img ref={ref} id="custom-gallery-item" onClick={open} src={image.thumbnail} />
+                    </div>
                   )}
                 </Item>
               )
@@ -156,10 +121,15 @@ function App() {
       </div>
       <form>
         <div id="upload-image-container">
+          <h1 id="upload-backdrop-text">UPLOAD</h1>
           <input id="image-upload" type="file" accept="image/*" id="choose-file" name="choose-file" onChange={readFile} />
         </div>
       </form>
-
+      <div id="small-footer">
+        <p>
+          Bildergalerie - Unsplash x Lewin Gerber 
+        </p>
+      </div>
     </div>
   );
 }
